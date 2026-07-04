3623d61ecf2f32303ed9a7814a5a3acd08a279ef
Howard <hmt0310@gmail.com>
2026-07-04 19:07:20 -0400

    Corregir configuración de Vite y Tailwind

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import vue from '@vitejs/plugin-vue'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [vue(),tailwindcss(),],
- base: import.meta.env.BASE_URL,
+  plugins: [
+    vue(),
+    tailwindcss()
+  ],
+  base: '/appVueclase4/',
 })
