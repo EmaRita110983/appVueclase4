ee320bd20d6e522d44b0646f697612a3eccee13f
Howard <hmt0310@gmail.com>
2026-07-05 13:05:05 -0400

    Hash actualizado

--- src/views/Producto.vue
+++ src/views/Producto.vue
@@ -32,7 +32,7 @@
               placeholder="Ej: 1200.00" />
           </div>
           <div>
-            <label for="imagen" class="block text-sm font-medium text-gray-700" width="100%">URL de la Imagen:</label>
+            <label for="imagen" class="block text-sm font-medium text-gray-700" width="50%">URL de la Imagen:</label>
             <input type="text" id="imagen" v-model="producto.imagen"
               class="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
               placeholder="Ej: https://ejemplo.com/imagen.jpg" />
